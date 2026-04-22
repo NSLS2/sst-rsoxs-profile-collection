095f61c38007de812f87f9a908c372eb88e2df86
Charles Titus <ctitus@bnl.gov>
2026-04-22 14:38:01 -0400

    Sim working locally

--- scripts/qs-start.sh
+++ scripts/qs-start.sh
@@ -3,4 +3,4 @@ set -e
 set -o xtrace
 pip install -e /home/xf07id1/collection_packages/rsoxs
 
-start-re-manager --zmq-publish-console ON --use-ipython-kernel ON --ipython-kernel-ip auto --startup-profile collection
+start-re-manager --redis-addr ${QS_REDIS_ADDR:-localhost}:6379 --zmq-publish-console ON --use-ipython-kernel ON --ipython-kernel-ip auto --startup-profile collection
