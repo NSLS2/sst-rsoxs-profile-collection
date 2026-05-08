3b7dd3afc08efef62cea6694a4cea693fc9a5e8d
Charles Titus <ctitus@bnl.gov>
2026-05-08 16:59:15 -0400

    Modifying the dev settings a bit. Do not need to install sst-base currently, and the dev compose file should run qs-dev

--- scripts/qs-dev.sh
+++ scripts/qs-dev.sh
@@ -2,7 +2,7 @@
 set -e
 set -o xtrace
 
-pip install -e /home/xf07id1/collection_packages/sst-base
+# pip install -e /home/xf07id1/collection_packages/sst-base
 pip install -e /home/xf07id1/collection_packages/rsoxs
 
 $(dirname "$0")/qs-start.sh
