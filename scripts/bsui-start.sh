c5f712c6570bc2901033c577a72a3629bf9110a3
SST Operator <xf07id1@xf07id-ws24.nsls2.bnl.local>
2026-06-02 14:02:24 -0400

    Updated which packages are used from editable installs on beamline computer.

--- scripts/bsui-start.sh
+++ scripts/bsui-start.sh
@@ -2,8 +2,8 @@
 set -e
 set -o xtrace
 
-pip install -e /home/xf07id1/xraygui/nbs-bl
-pip install -e /home/xf07id1/xraygui/sst_base
+#pip install -e /home/xf07id1/xraygui/nbs-bl
+#pip install -e /home/xf07id1/xraygui/sst_base
 pip install -e /home/xf07id1/collection_packages/rsoxs
 
 unset SESSION_MANAGER PYTHONPATH && MPLBACKEND=qtagg ipython --profile-dir=. 
