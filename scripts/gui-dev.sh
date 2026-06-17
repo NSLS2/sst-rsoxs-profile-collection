915120f7d36d9d015b6763bb016c2da7a5c65723
Charles Titus <ctitus@bnl.gov>
2026-06-17 17:01:18 -0400

    Updating for new mode object

--- scripts/gui-dev.sh
+++ scripts/gui-dev.sh
@@ -2,7 +2,8 @@
 set -e
 set -o xtrace
 
-pip install -e /home/xf07id1/xraygui/nbs-gui
+pip install -e /home/xf07id1/collection_packages/nbs-gui
+pip install -e /home/xf07id1/collection_packages/nbs-bl
 pip install -e /home/xf07id1/collection_packages/rsoxs
 
 
--- scripts/qs-dev.sh
+++ scripts/qs-dev.sh
@@ -3,6 +3,7 @@ set -e
 set -o xtrace
 
 # pip install -e /home/xf07id1/collection_packages/sst-base
+pip install -e /home/xf07id1/collection_packages/nbs-bl
 pip install -e /home/xf07id1/collection_packages/rsoxs
 
 $(dirname "$0")/qs-start.sh
